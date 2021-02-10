5e36dc2a2c23198bccc18e7c489c4805905f46b0
Sydney M <smoncur0@email.cpcc.edu>
2021-02-10 15:07:37 -0500

    Create HelloWorld.java
    Create HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;// import the Scanner class
+public class HelloWorld
+{
+   public static void main(String[] args)
+   { 
+   
+     System.out.println( Hello World! );
+      
+         
+         
+         
+         
+         
+         
+                    
+              
+      
